8374bbbfb630ab55657e45792c446cede0f47ce0
wangzhuoran <365866220@qq.com>
2019-11-20 20:12:48 +0800

    yi

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<template>
+	<div class="hello">
+		<van-button type="default">默认按钮</van-button>
+		<van-button type="primary">主要按钮</van-button>
+		<van-button type="info">信息按钮</van-button>
+		<van-button type="warning">警告按钮</van-button>
+		<van-button type="danger">危险按钮</van-button>
+
+
+		<van-cell is-link @click="showPopup">展示弹出层</van-cell>
+		<van-popup v-model="show" position="top" :style="{ height: '20%' }" />
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'HelloWorld',
+		props: {
+			// msg: String,
+			show: false
+		},
+		methods: {
+			showPopup() {
+				this.show = true;
+			}
+		}
+	}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+	h3 {
+		margin: 40px 0 0;
+	}
+
+	ul {
+		list-style-type: none;
+		padding: 0;
+	}
+
+	li {
+		display: inline-block;
+		margin: 0 10px;
+	}
+
+	a {
+		color: #42b983;
+	}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="about">
+		<h1>This is an about page</h1>
+
+
+		<van-button @touchstart.stop="show = true">
+			弹出默认键盘
+		</van-button>
+
+		<van-number-keyboard v-model="value" :show="show" :maxlength="6" @blur="show = false" />
+		<van-number-keyboard :show="show" extra-key="." close-button-text="完成" @blur="show = false" @input="onInput" @delete="onDelete" />
+	</div>
+</template>
+
+
+<script>
+	export default {
+		data() {
+			return {
+				show: false,
+				value: ''
+			}
+		},
+
+		methods: {
+			onInput(value) {
+				Toast(value);
+			},
+			onDelete() {
+				Toast('删除');
+			}
+		}
+	}
+</script>
